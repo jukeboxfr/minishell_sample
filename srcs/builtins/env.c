d5bdfe7af3a86e57435182964518de48901f698d
Keny SAINT HILAIRE <kesaint-@e1r5p4.42.fr>
2019-07-09 14:42:27 +0200

    Update

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -6,18 +6,56 @@
 /*   By: kesaint- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/08 14:04:08 by kesaint-          #+#    #+#             */
-/*   Updated: 2019/06/10 19:24:21 by kesaint-         ###   ########.fr       */
+/*   Updated: 2019/07/09 14:42:09 by kesaint-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
-void	builtin_env(int argc, char **argv, t_var **envp)
+static t_bool	is_valid_input(char *start, char *end)
+{
+	end--;
+	while (end != start)
+	{
+		if (!ft_isalnum(*end) && *end != '_')
+			return (FALSE);
+		end--;
+	}
+	return (TRUE);
+}
+
+static void		set_env_vars(char *argv[], t_var **envp)
+{
+	char	*ptr;
+	char	*value;
+	char	c;
+
+	argv++;
+	while (*argv)
+	{
+		if ((ptr = ft_strchr(*argv, '=')) && is_valid_input(*argv, ptr))
+		{
+			c = *ptr;
+			*ptr = '\0';
+			if (!(value = ft_strdup(ptr + 1)))
+			{
+				*ptr = c;
+				return ;
+			}
+			edit_var(envp, *argv, value);
+			*ptr = c;
+		}
+		argv++;
+	}
+}
+
+void			builtin_env(int argc, char **argv, t_var **envp)
 {
 	t_var	*env;
 
 	(void)argc;
 	(void)envp;
+	set_env_vars(argv, envp);
 	env = *envp;
 	while (env)
 	{
